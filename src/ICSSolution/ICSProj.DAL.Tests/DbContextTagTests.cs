2f4be2122ea14ae50da6126f699b09a6e37561b8
Jaroslav Streit <Jarek.Streit@seznam.cz>
2023-03-03 10:53:22 +0100

    Tag tests final

--- src/ICSSolution/ICSProj.DAL.Tests/DbContextTagTests.cs
+++ src/ICSSolution/ICSProj.DAL.Tests/DbContextTagTests.cs
@@ -61,8 +61,8 @@ public class DbContextTagTests : DbContextTestsBase
                     Id = Guid.Parse("d524977c-a212-4289-bb2f-431b6e9c5725"),
                     CreatorId = UserSeeds.UserEntity1.Id,
                     Description = "First activity",
-                    Start = new DateTime(2023, 2, 10, 12,0,0),
-                    End = new DateTime(2023, 2,10,13,0,0),
+                    Start = new DateTime(2023, 2, 10, 12, 0, 0),
+                    End = new DateTime(2023, 2, 10, 13, 0, 0),
                     ProjectId = default,
                     TagId = default
                 },
@@ -71,8 +71,8 @@ public class DbContextTagTests : DbContextTestsBase
                     Id = Guid.Parse("00b23592-cdf5-4c0f-ba87-0da92152e5c5"),
                     CreatorId = UserSeeds.UserEntity2.Id,
                     Description = "Second activity",
-                    Start = new DateTime(2023, 2, 10, 10,0,0),
-                    End = new DateTime(2023, 2,10,11,0,0),
+                    Start = new DateTime(2023, 2, 10, 10, 0, 0),
+                    End = new DateTime(2023, 2, 10, 11, 0, 0),
                     ProjectId = default,
                     TagId = default
                 }
@@ -91,10 +91,22 @@ public class DbContextTagTests : DbContextTestsBase
     }
 
     [Fact]
+
+    public async Task GetById_IncludingActivities_Tag()
+    {
+        var entity = await ICSProjDbContextSUT.Tags
+            .Include(i => i.Activities)
+            .SingleAsync(i => i.Id == TagSeeds.TagEntity1.Id);
+
+        //Assert
+        DeepAssert.Equal(TagSeeds.TagEntity1, entity);
+    }
+
+    [Fact]
     public async Task Update_Tag()
     {
         var baseEntity = TagSeeds.TagEntityUpdate;
-        var entity = baseEntity with { Name = "To be deleted" };
+        var entity = baseEntity with { Name = "Updated name" };
 
         ICSProjDbContextSUT.Tags.Update(entity);
         await ICSProjDbContextSUT.SaveChangesAsync();
@@ -107,13 +119,28 @@ public class DbContextTagTests : DbContextTestsBase
     }
 
     [Fact]
-    public async Task Delete_RecipeWithoutIngredients_Deleted()
+    public async Task Delete_Tag()
     {
         //Arrange
         var baseEntity = TagSeeds.TagEntityDelete;
 
         //Act
         ICSProjDbContextSUT.Tags.Remove(baseEntity);
+        await ICSProjDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        Assert.False(await ICSProjDbContextSUT.Tags.AnyAsync(i => i.Id == baseEntity.Id));
+    }
+
+    [Fact]
+    public async Task DeleteById_Tag()
+    {
+        //Arrange
+        var baseEntity = TagSeeds.TagEntityDelete;
+
+        //Act
+        ICSProjDbContextSUT.Remove(
+            ICSProjDbContextSUT.Tags.Single(i => i.Id == baseEntity.Id));
         await ICSProjDbContextSUT.SaveChangesAsync();
 
         //Assert
