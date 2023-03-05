af2be5a2464a574d94f6bf8a27bd98f34087c524
JachymD <jachym.dolezal@gmail.com>
2023-03-05 19:01:52 +0100

    basic project tests

--- src/ICSSolution/ICSProj.Common.Tests/Seeds/ProjectSeeds.cs
+++ src/ICSSolution/ICSProj.Common.Tests/Seeds/ProjectSeeds.cs
@@ -15,15 +15,30 @@ public static class ProjectSeeds
 
     public static readonly ProjectEntity ProjectEntity1 = new()
     {
-        Id = Guid.Parse("a5ac05fd-daae-4109-b5dc-09793b82ab61"),
+        Id = Guid.Parse("2a6901cd-7519-43ee-a43c-f7e017498910"),
         Name = "Project 1",
         CreatorId = UserSeeds.UserEntity1.Id
     };
 
+    public static readonly ProjectEntity ProjectEntity2 = new()
+    {
+        Id = Guid.Parse("90042db0-4bfa-4eef-bbf8-67c4ffa59a98"),
+        Name = "Project 2",
+        CreatorId = UserSeeds.UserEntity2.Id
+    };
+
+    public static readonly ProjectEntity ProjectDelete =
+        ProjectEntity1 with { Id = Guid.Parse("1a0b54ce-4182-439a-851a-cfb0173c679f") };
+    public static readonly ProjectEntity ProjectUpdate =
+        ProjectEntity1 with { Id = Guid.Parse("34d552e4-1b52-4288-afe6-26a448964801") };
+
     public static void Seed(this ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ProjectEntity>().HasData(
-            ProjectEntity1 with {Creator = null, ProjectAssigns = Array.Empty<ProjectAssignEntity>()}
+            ProjectEntity1 with {Creator = null, ProjectAssigns = Array.Empty<ProjectAssignEntity>(), Activities = Array.Empty<ActivityEntity>()},
+            ProjectEntity2 with { Creator = null, ProjectAssigns = Array.Empty<ProjectAssignEntity>(), Activities = Array.Empty<ActivityEntity>() },
+            ProjectDelete with { Creator = null, ProjectAssigns = Array.Empty<ProjectAssignEntity>(), Activities = Array.Empty<ActivityEntity>() },
+            ProjectUpdate with { Creator = null, ProjectAssigns = Array.Empty<ProjectAssignEntity>(), Activities = Array.Empty<ActivityEntity>() }
         );
     }
 }
---
+++ src/ICSSolution/ICSProj.DAL.Tests/DbContextProjectTests.cs
@@ -0,0 +1,104 @@
+using System.Runtime.InteropServices.JavaScript;
+using ICSProj.DAL.Entities;
+using Xunit;
+using Xunit.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using ICSProj.Common.Tests;
+using ICSProj.Common.Tests.Seeds;
+using System.Globalization;
+
+namespace ICSProj.DAL.Tests;
+
+public class DbContextProjectTests : DbContextTestsBase
+{
+    public DbContextProjectTests(ITestOutputHelper output) : base(output)
+    {
+    }
+
+    //add new project
+    [Fact]
+    public async Task Add_Project()
+    {
+        var entity = ProjectSeeds.EmptyProjectEntity with
+        {
+            CreatorId = UserSeeds.UserEntity1.Id,
+            Activities = Array.Empty<ActivityEntity>(),
+            ProjectAssigns = Array.Empty<ProjectAssignEntity>(),
+            Name = ProjectSeeds.ProjectEntity1.Name,
+
+        };
+
+        ICSProjDbContextSUT.Projects.Add(entity);
+        await ICSProjDbContextSUT.SaveChangesAsync();
+
+
+        //Asert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Projects
+            .SingleAsync(i => i.Id == entity.Id);
+        DeepAssert.Equal(entity, actualEntity);
+
+    }
+
+    //get project by id
+    [Fact]
+    public async Task GetById_Project()
+    {
+        //Act
+        var entity = await ICSProjDbContextSUT.Projects.SingleAsync(i => i.Id == ProjectSeeds.ProjectEntity1.Id);
+
+        //Assert
+        DeepAssert.Equal(ProjectSeeds.ProjectEntity1, entity);
+    }
+
+    //update project update
+    [Fact]
+    public async Task Update_Project()
+    {
+        //Arrange
+        var baseEntity = ProjectSeeds.ProjectUpdate;
+        var entity = baseEntity with {Name = baseEntity.Name + "Updated"};
+
+        //Act
+        ICSProjDbContextSUT.Projects.Update(entity);
+        await ICSProjDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        var actualEntity = await dbx.Projects.SingleAsync(i => i.Id == entity.Id);
+
+        DeepAssert.Equal(entity, actualEntity);
+    }
+
+    //delete project
+    [Fact]
+    public async Task Delete_Project()
+    {
+        var entityBase = ProjectSeeds.ProjectDelete;
+
+
+        //Act
+        ICSProjDbContextSUT.Projects.Remove(entityBase);
+        await ICSProjDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        Assert.False(await ICSProjDbContextSUT.Projects.AnyAsync(i => i.Id == entityBase.Id));
+    }
+
+    //delete by id
+    [Fact]
+    public async Task DeleteById_Project()
+    {
+        //Arrange
+        var entityBase = ProjectSeeds.ProjectDelete;
+
+        //Act
+        ICSProjDbContextSUT.Remove(
+            ICSProjDbContextSUT.Projects.Single(i => i.Id == entityBase.Id));
+        await ICSProjDbContextSUT.SaveChangesAsync();
+
+        //Assert
+        Assert.False(await ICSProjDbContextSUT.Projects.AnyAsync(i => i.Id == entityBase.Id));
+    }
+
+}
