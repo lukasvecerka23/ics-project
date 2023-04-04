3985a6297dbd5715b2269d642d81aded7de4b238
Alina <xpenko00@vutbr.cz>
2023-04-04 22:43:06 +0200

    implement base facade v.1

---
+++ src/ICSSolution/ICSProj.BL/Facades/FacadeBase.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using ICSProj.BL.Facades.Interfaces;
+using ICSProj.BL.Mappers;
+using ICSProj.BL.Models;
+using ICSProj.DAL.Entities;
+using ICSProj.DAL.Mappers;
+using ICSProj.DAL.Repositories;
+using ICSProj.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace ICSProj.BL.Facades;
+
+public abstract class
+    FacadeBase<TEntity, TListModel, TDetailModel, TEntityMapper> : IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+    where TEntityMapper : IEntityMapper<TEntity>, new()
+{
+    protected readonly IModelMapper<TEntity, TListModel, TDetailModel> ModelMapper;
+    protected readonly IUnitOfWorkFactory UnitOfWorkFactory;
+
+    protected FacadeBase(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IModelMapper<TEntity, TListModel, TDetailModel> modelMapper)
+    {
+        UnitOfWorkFactory = unitOfWorkFactory;
+        ModelMapper = modelMapper;
+    }
+
+    //protected virtual string IncludesNavigationPathDetail => string.Empty;
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+
+        uow.GetRepository<TEntity, TEntityMapper>().Delete(id);
+        await uow.CommitAsync().ConfigureAwait(false);
+    }
+    
+    public virtual async Task<TDetailModel> SaveAsync(TDetailModel model)
+    {
+        TDetailModel result;
+
+        TEntity entity = ModelMapper.MapToEntity(model);
+
+        IUnitOfWork uow = UnitOfWorkFactory.Create();
+        IRepository<TEntity> repository = uow.GetRepository<TEntity, TEntityMapper>();
+
+        if (await repository.ExistsAsync(entity))
+        {
+            TEntity updatedEntity = await repository.UpdateAsync(entity);
+            result = ModelMapper.MapToDetailModel(updatedEntity);
+        }
+        else
+        {
+            entity.Id = Guid.NewGuid();
+            TEntity insertedEntity = await repository.InsertAsync(entity);
+            result = ModelMapper.MapToDetailModel(insertedEntity);
+        }
+
+        await uow.CommitAsync();
+
+        return result;
+    }
+
+    public virtual async Task<TDetailModel?> GetAsync(Guid id)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+
+        IRepository<TEntity> rep = uow.GetRepository<TEntity, TEntityMapper>();
+        TEntity entity = await rep.Get().SingleOrDefaultAsync(e => e.Id == id);
+
+        return entity == null ? null : ModelMapper.MapToDetailModel(entity);
+    }
+
+    public virtual async Task<IEnumerable<TListModel>> GetAsync()
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+
+        List<TEntity> entities = await uow
+            .GetRepository<TEntity, TEntityMapper>()
+            .Get()
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+}
