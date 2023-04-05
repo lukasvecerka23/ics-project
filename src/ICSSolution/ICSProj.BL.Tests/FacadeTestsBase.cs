bd945f5ce0baa7def0760fa8c87851977216111a
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-04-05 14:03:37 +0200

    implement facade tests base

---
+++ src/ICSSolution/ICSProj.BL.Tests/FacadeTestsBase.cs
@@ -0,0 +1,70 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using ICSProj.BL.Mappers;
+using ICSProj.Common.Tests;
+using ICSProj.Common.Tests.Factories;
+using Microsoft.EntityFrameworkCore;
+using ICSProj.DAL;
+using ICSProj.DAL.Mappers;
+using ICSProj.DAL.UnitOfWork;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ICSProj.BL.Tests;
+
+public class FacadeTestsBase: IAsyncLifetime
+{
+    protected FacadeTestsBase(ITestOutputHelper output)
+    {
+        XUnitTestOutputConverter converter = new(output);
+        Console.SetOut(converter);
+
+        DbContextFactory = new DbContextSQLiteTestingFactory(GetType().FullName!, seedTestingData: true);
+
+        UserEntityMapper = new UserEntityMapper();
+        TagEntityMapper = new TagEntityMapper();
+        ActivityEntityMapper = new ActivityEntityMapper();
+        ProjectEntityMapper = new ProjectEntityMapper();
+        ProjectAssignEntityMapper = new ProjectAssignEntityMapper();
+
+
+
+        ActivityModelMapper = new ActivityModelMapper();
+        ProjectAssignModelMapper = new ProjectAssignModelMapper();
+        TagModelMapper = new TagModelMapper(ActivityModelMapper);
+        ProjectModelMapper = new ProjectModelMapper(ActivityModelMapper, ProjectAssignModelMapper);
+        UserModelMapper = new UserModelMapper(ProjectAssignModelMapper);
+
+        UnitOfWorkFactory = new UnitOfWorkFactory(DbContextFactory);
+    }
+
+    protected IDbContextFactory<ICSProjDbContext> DbContextFactory { get; }
+
+    protected UserEntityMapper UserEntityMapper { get; }
+    protected TagEntityMapper TagEntityMapper { get; }
+    protected ActivityEntityMapper ActivityEntityMapper { get; }
+    protected ProjectEntityMapper ProjectEntityMapper { get; }
+    protected ProjectAssignEntityMapper ProjectAssignEntityMapper { get; }
+
+    protected IUserModelMapper UserModelMapper { get; }
+    protected ITagModelMapper TagModelMapper { get; }
+    protected IActivityModelMapper ActivityModelMapper { get; }
+    protected IProjectModelMapper ProjectModelMapper { get; }
+    protected IProjectAssignModelMapper ProjectAssignModelMapper { get; }
+
+    protected UnitOfWorkFactory UnitOfWorkFactory { get; }
+
+    public async Task InitializeAsync()
+    {
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        await dbx.Database.EnsureCreatedAsync();
+        await dbx.Database.EnsureCreatedAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await using var dbx = await DbContextFactory.CreateDbContextAsync();
+        await dbx.Database.EnsureDeletedAsync();
+    }
+
+}
