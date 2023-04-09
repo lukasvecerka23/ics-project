7fcb246bf054a7eda51fa6df532be8c902957d61
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-04-09 15:27:02 +0200

    implementation of getasync for userfacade - include project assigns

--- src/ICSSolution/ICSProj.BL/Facades/UserFacade.cs
+++ src/ICSSolution/ICSProj.BL/Facades/UserFacade.cs
@@ -6,6 +6,7 @@ using ICSProj.BL.Models;
 using ICSProj.DAL.Entities;
 using ICSProj.DAL.Mappers;
 using ICSProj.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
 
 namespace ICSProj.BL.Facades;
 
@@ -18,5 +19,17 @@ public class UserFacade: FacadeBase<UserEntity, UserListModel, UserDetailModel, 
         IUserModelMapper userModelMapper) : base(unitOfWorkFactory, userModelMapper) =>
         _userModelMapper = userModelMapper;
 
+    public override async Task<UserDetailModel?> GetAsync(Guid id)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
 
+        IQueryable<UserEntity> query = uow.GetRepository<UserEntity, UserEntityMapper>().Get();
+
+        UserEntity? entity = await query
+            .Include(e => e.ProjectAssigns)
+            .ThenInclude(p => p.Project)
+            .SingleOrDefaultAsync(e => e.Id == id);
+
+        return entity == null ? null : _userModelMapper.MapToDetailModel(entity);
+    }
 }
