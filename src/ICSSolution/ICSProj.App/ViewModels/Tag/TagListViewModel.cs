81f6bf728c27a7dc7fc1375b15a5295e87fe11e3
Jaroslav Streit <Jarek.Streit@seznam.cz>
2023-05-04 17:26:25 +0200

    Add tag list viewmodel

--- src/ICSSolution/ICSProj.App/ViewModels/Tag/TagListViewModel.cs
+++ src/ICSSolution/ICSProj.App/ViewModels/Tag/TagListViewModel.cs
@@ -1,9 +1,32 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using ICSProj.App.Services;
+using ICSProj.BL.Facades;
+using ICSProj.BL.Models;
 
-namespace ICSProj.App.ViewModels.Tag;
+namespace ICSProj.App.ViewModels;
 
-public class TagListViewModel
+public partial class TagListViewModel : ViewModelBase
 {
-    
+    private readonly ITagFacade _tagFacade;
+    private readonly INavigationService _navigationService;
+    private readonly ILoginService _loginService;
+
+    public TagListViewModel(
+        ITagFacade tagFacade,
+        INavigationService navigationService,
+        ILoginService loginService,
+        IMessengerService messengerService) : base(messengerService)
+    {
+        _tagFacade = tagFacade;
+        _navigationService = navigationService;
+        _loginService = loginService;
+    }
+
+    public IEnumerable<TagListModel> Tags { get; set; } = null!;
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+        var tags = await _tagFacade.GetAsync();
+        Tags = tags.Where(tag => tag.CreatorId == _loginService.CurrentUserId);
+    }
 }
