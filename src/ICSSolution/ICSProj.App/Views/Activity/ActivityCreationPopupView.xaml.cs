fc48eacc48745a899f6f93cff981f0d6bef1c5ac
Alina <xpenko00@vutbr.cz>
2023-05-15 06:15:25 +0200

    add activity creation popup menu

---
+++ src/ICSSolution/ICSProj.App/Views/Activity/ActivityCreationPopupView.xaml.cs
@@ -0,0 +1,16 @@
+using CommunityToolkit.Maui.Views;
+
+namespace ICSProj.App.Views.Activity;
+
+public partial class ActivityCreationPopupView : Popup
+{
+    public ActivityCreationPopupView()
+    {
+        InitializeComponent();
+    }
+
+    private void ClosePopup(object sender, EventArgs e)
+    {
+        Close();
+    }
+}
